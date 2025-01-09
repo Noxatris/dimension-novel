acf866d9fecd19129320821de0a6a05f0d84c3f9
Noxatris <81755314+Noxatris@users.noreply.github.com>
2025-01-09 00:21:54 -0400

    Add responsivity for landing image

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen overflow-hidden bg-cover relative" style={{ backgroundImage: 'url(/bgMain.jpg)' }}>
-      <div className="w-screen h-screen bg-cover animate-[blurEffectSide_4s_ease-in-out_infinite_alternate] absolute" style={{ backgroundImage: 'url(/bgOverSide.png)' }}></div>
-      <div className="w-screen h-screen bg-cover animate-[blurEffectMiddle_4s_ease-in-out_infinite_alternate] absolute" style={{ backgroundImage: 'url(/bgOverMiddle.png)' }}></div>
+    <div className="w-screen overflow-x-hidden">
+      <div className="w-full h-screen bg-center bg-cover relative" style={{ backgroundImage: 'url(/bgMain.jpg)' }}>
+        <div className="w-full h-screen bg-center bg-cover animate-[blurEffectSide_4s_ease-in-out_infinite_alternate] absolute" style={{ backgroundImage: 'url(/bgOverSide.png)' }}></div>
+        <div className="w-full h-screen bg-center bg-cover animate-[blurEffectMiddle_4s_ease-in-out_infinite_alternate] absolute" style={{ backgroundImage: 'url(/bgOverMiddle.png)' }}></div>
+        <div>
+          <h1>Dimension Novel</h1>
+        </div>
+      </div>
+      <div>
+      </div>
     </div>
   );
 }
