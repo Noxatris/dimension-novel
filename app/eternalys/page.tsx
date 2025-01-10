0c86df494d4f8124a0842d8ecb0d6ab4f6210f72
Noxatris <81755314+Noxatris@users.noreply.github.com>
2025-01-10 01:56:48 -0400

    Initialisation Eternalys

--- app/eternalys/page.tsx
+++ app/eternalys/page.tsx
@@ -1,13 +1,15 @@
 export default function EternalysMain() {
   return (
-    <div className='w-full h-[90vh] bg-center bg-cover bg-fixed flex justify-center' style={{ backgroundImage: 'url(/bgMain.jpg)' }}>
-      <div className='w-[85%] bg-black/55 p-10 h-[2000px] bg-local'>
-        <div>
-          <h1>Eternalys : Éveil et Vengeance</h1>
-        </div>
-        <div>
-          <h2>Intrigue :</h2>
-          <p>Une description géniale et captivante en devenir</p>
+    <div className='w-full h-screen bg-center bg-cover bg-fixed flex justify-center pt-[9vh]' style={{ backgroundImage: 'url(/bgMain.jpg)' }}>
+      <div className='w-full bg-black/55 flex flex-col items-center bg-top bg-cover' style={{ backgroundImage: 'url(cover.png)' }}>
+        <div className="w-screen h-screen bg-black/80 pt-8">
+          <div>
+            <h1>Eternalys : Éveil et Vengeance</h1>
+          </div>
+          <div>
+            <h2>Intrigue :</h2>
+            <p>Une description géniale et captivante en devenir</p>
+          </div>
         </div>
       </div>
     </div>
