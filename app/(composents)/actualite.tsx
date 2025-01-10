409fc7e2a03e54540c5abb9f77bd383a753c43c0
Noxatris <81755314+Noxatris@users.noreply.github.com>
2025-01-10 01:01:17 -0400

    Finally...

--- app/(composents)/actualite.tsx
+++ app/(composents)/actualite.tsx
@@ -7,12 +7,12 @@ export default function Actualite() {
             </div>
             <div className="flex flex-col overflow-y-scroll">
             <div className="border-l-4 rounded border-violet-700 px-3 mb-6">
-                <h3 className="text-xl font-[MedievalSharp] border-b-2 mb-3">Le titre de l&aposactu</h3>
-                <p>Et les information de l&aposactu evidemment toute incroyable et exeptionnelle et j&aposécris pas tout ça juste pour remplir, enfin peut etre que si finalement</p>
+                <h3 className="text-xl font-[MedievalSharp] border-b-2 mb-3">Le titre de l&apos;actu</h3>
+                <p>Et les information de l&apos;actu evidemment toute incroyable et exeptionnelle et j&apos;écris pas tout ça juste pour remplir, enfin peut etre que si finalement</p>
             </div>
             <div className="border-l-4 rounded border-violet-700 px-3">
                 <h3 className="text-xl font-[MedievalSharp] border-b-2 mb-3">Une autre actu pour test le scrolling</h3>
-                <p>Je fais genre j&aposécris un truk mé en vrai tt le monde sen fou é c just pr tst</p>
+                <p>Je fais genre j&apos;écris un truk mé en vrai tt le monde sen fou é c just pr tst</p>
             </div>
             </div>
 
