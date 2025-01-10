cae9000da5ef8cb88c794a0064721d68d03a39a4
Noxatris <81755314+Noxatris@users.noreply.github.com>
2025-01-10 01:07:10 -0400

    Test new bg actu

--- app/(composents)/actualite.tsx
+++ app/(composents)/actualite.tsx
@@ -5,15 +5,15 @@ export default function Actualite() {
             <div className="relative w-[50%] h-2 bg-yellow-300 rounded flex items-center mb-3">
                 <div className="absolute w-1 h-1 rounded-full animate-dotMove shadow-dotEffect"></div>
             </div>
-            <div className="flex flex-col overflow-y-scroll">
-            <div className="border-l-4 rounded border-violet-700 px-3 mb-6">
-                <h3 className="text-xl font-[MedievalSharp] border-b-2 mb-3">Le titre de l&apos;actu</h3>
-                <p>Et les information de l&apos;actu evidemment toute incroyable et exeptionnelle et j&apos;écris pas tout ça juste pour remplir, enfin peut etre que si finalement</p>
-            </div>
-            <div className="border-l-4 rounded border-violet-700 px-3">
-                <h3 className="text-xl font-[MedievalSharp] border-b-2 mb-3">Une autre actu pour test le scrolling</h3>
-                <p>Je fais genre j&apos;écris un truk mé en vrai tt le monde sen fou é c just pr tst</p>
-            </div>
+            <div className="flex flex-col overflow-y-scroll pb-2">
+                <div className="border-l-4 rounded-xl border-violet-700 px-3 mb-6 bg-black/60">
+                    <h3 className="text-xl font-[MedievalSharp] border-b-2 mb-3">Le titre de l&apos;actu</h3>
+                    <p>Et les information de l&apos;actu evidemment toute incroyable et exeptionnelle et j&apos;écris pas tout ça juste pour remplir, enfin peut etre que si finalement</p>
+                </div>
+                <div className="border-l-4 rounded-xl border-violet-700 px-3">
+                    <h3 className="text-xl font-[MedievalSharp] border-b-2 mb-3">Une autre actu pour test le scrolling</h3>
+                    <p>Je fais genre j&apos;écris un truk mé en vrai tt le monde sen fou é c just pr tst</p>
+                </div>
             </div>
 
 
