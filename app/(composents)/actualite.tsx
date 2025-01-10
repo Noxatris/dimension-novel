b811aa8b40de47a6df5b8806301a763952b43b33
Noxatris <81755314+Noxatris@users.noreply.github.com>
2025-01-10 01:14:14 -0400

    New try for actu bg

--- app/(composents)/actualite.tsx
+++ app/(composents)/actualite.tsx
@@ -6,11 +6,11 @@ export default function Actualite() {
                 <div className="absolute w-1 h-1 rounded-full animate-dotMove shadow-dotEffect"></div>
             </div>
             <div className="flex flex-col overflow-y-scroll pb-2">
-                <div className="border-l-4 rounded-xl border-violet-700 px-3 mb-6 bg-black/60">
+                <div className="border-l-4 rounded-xl border-violet-700 px-3 py-1 mb-6 bg-black/50">
                     <h3 className="text-xl font-[MedievalSharp] border-b-2 mb-3">Le titre de l&apos;actu</h3>
                     <p>Et les information de l&apos;actu evidemment toute incroyable et exeptionnelle et j&apos;écris pas tout ça juste pour remplir, enfin peut etre que si finalement</p>
                 </div>
-                <div className="border-l-4 rounded-xl border-violet-700 px-3">
+                <div className="border-l-4 rounded-xl border-violet-700 px-3 py-1 bg-black/50">
                     <h3 className="text-xl font-[MedievalSharp] border-b-2 mb-3">Une autre actu pour test le scrolling</h3>
                     <p>Je fais genre j&apos;écris un truk mé en vrai tt le monde sen fou é c just pr tst</p>
                 </div>
