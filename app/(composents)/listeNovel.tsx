7b27a519caebb17905ef9fe517eac4c870ea2f6d
Noxatris <81755314+Noxatris@users.noreply.github.com>
2025-01-12 15:28:01 -0400

    Adjust link width in MesNovels component and reduce radial wave animation duration

--- app/(composents)/listeNovel.tsx
+++ app/(composents)/listeNovel.tsx
@@ -8,7 +8,7 @@ export default function MesNovels() {
         <div className="w-1 h-1 rounded-full animate-dotMove shadow-dotEffect"></div>
       </div>
       <div className="flex justify-center mb-[48px]">
-        <Link href="/eternalys" className='flex flex-col w-full md:w-[60%] lg:w-[40%] rounded-xl overflow-hidden items-center pulse-animation pointer-cursor'>
+        <Link href="/eternalys" className='flex flex-col w-10/12 md:w-[60%] lg:w-[40%] rounded-xl overflow-hidden items-center pulse-animation pointer-cursor'>
           <img src="/cover.png" alt="Cover" className='w-full h-auto' />
         </Link>
       </div>
