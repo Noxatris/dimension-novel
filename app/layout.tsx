381b5b4c98f94c4b5c1ccff6d94c6f7b24409f3c
Noxatris <81755314+Noxatris@users.noreply.github.com>
2025-01-13 00:37:20 -0400

    Refactor layout with updated gradient animations, logo size adjustments, and new font integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,13 +25,12 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className="antialiased overflow-x-hidden w-screen">
-        <header className="relative w-screen shadow-2xl p-4 md:p-8">
+        <header className="relative w-screen shadow-2xl p-4 md:p-8 bg-gradient-to-b from-violet-900 to-gray-900">
           {/* Background with gradient and animation */}
-          <div className="absolute inset-0 bg-gradient-to-br from-violet-900 via-gray-900 via-violet-900 to-black animate-gradientMove"></div>
           <div className="relative z-10 flex items-center justify-between">
             <div className="flex items-center">
-              <img src="/logoAlone.png" alt="Logo" className="h-[55px] md:h-[75px]" />
-              <h1 className="text-3xl md:text-4xl ml-2 font-[MedievalSharp]">Dimension Novels</h1>
+              <img src="/logoAlone.png" alt="Logo" className="h-[45px] md:h-[65px]" />
+              <h1 className="text-2xl md:text-3xl ml-2 medievalFont">Dimension Novels</h1>
             </div>
             <Link href="/" className="flex justify-center items-center p-2 h-12 w-12 rounded-full gradientMoveAnimation shadow-xl text-[1.2em] hover:bg-yellow-300 hover:text-black transition-all duration-300">
               <FontAwesomeIcon icon={faHome} className="fa-fw" />
