ab96d8a0829794f981c5d246354bd9bfd97a3397
Noxatris <81755314+Noxatris@users.noreply.github.com>
2025-03-11 01:18:14 -0400

    Add Header and Footer components to Actualité and Information pages

--- app/actualites/page.tsx
+++ app/actualites/page.tsx
@@ -1,4 +1,6 @@
 'use client'
+import Header from "../(composents)/header";
+import Footer from "../(composents)/footer";
 
 import { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
@@ -35,28 +37,32 @@ export default function Actualite() {
   }, []);
 
   return (
-    <div className="h-[45vh] md:h-[350px] lg:h-[35vh] flex flex-col md:items-center pt-8 px-4 md:px-8 lg:px-12 md:mb-8">
-      <h2 className="medievalFont text-xl md:text-2xl lg:text-3xl ml-6 md:ml-0">Actualité</h2>
-      <AnimateSeparator />
-      <div className="flex flex-col-reverse items-center overflow-y-scroll px-2 custom-scrollbar md:mt-2">
-        {loading ? (
-          <AltActualite />
-        ) : (
-          actualites.map((chapter: Chapter) => (
-            <div key={chapter.slug} className="md:w-[95%] border-l-4 rounded-xl border-violet-700 px-3 py-1 bg-black/50 mb-4">
-              <ReactMarkdown
-                components={{
-                  h1: ({ ...props }) => <h1 className="text-xl md:text-2xl font-[MedievalSharp] border-b-2 mb-3" {...props} />,
-                  h2: ({ ...props }) => <h2 className="text-3xl font-semibold" {...props} />,
-                  p: ({ ...props }) => <p className="text-base md:text-lg lg:text-xl" {...props} />,
-                }}
-              >
-                {chapter.content}
-              </ReactMarkdown>
-            </div>
-          ))
-        )}
+    <>
+      <Header />
+      <div className="h-[80vh] md:h-[350px] lg:h-[80vh] flex flex-col md:items-center pt-8 px-4 md:px-8 lg:px-12 md:mb-8">
+        <h2 className="medievalFont text-xl md:text-2xl lg:text-3xl ml-6 md:ml-0">Actualité</h2>
+        <AnimateSeparator />
+        <div className="flex flex-col-reverse items-center overflow-y-scroll px-2 custom-scrollbar md:mt-2">
+          {loading ? (
+            <AltActualite />
+          ) : (
+            actualites.map((chapter: Chapter) => (
+              <div key={chapter.slug} className="md:w-[95%] border-l-4 rounded-xl border-violet-700 px-3 py-1 bg-black/50 mb-4">
+                <ReactMarkdown
+                  components={{
+                    h1: ({ ...props }) => <h1 className="text-xl md:text-2xl font-[MedievalSharp] border-b-2 mb-3" {...props} />,
+                    h2: ({ ...props }) => <h2 className="text-3xl font-semibold" {...props} />,
+                    p: ({ ...props }) => <p className="text-base md:text-lg lg:text-xl" {...props} />,
+                  }}
+                >
+                  {chapter.content}
+                </ReactMarkdown>
+              </div>
+            ))
+          )}
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
--- app/informations/page.tsx
+++ app/informations/page.tsx
@@ -1,10 +1,14 @@
 import Header from "../(composents)/header";
+import Footer from "../(composents)/footer";
 
 export default function Information() {
     return (
         <>
             <Header />
-            <h1>Information</h1>
+            <div className="h-[80vh]">
+                <h1>Information</h1>
+            </div>
+            <Footer />
         </>
     )
 }
